e9c8970326b3347f7fcce2838522c87eb88dd190
asarium <asarium@gmail.com>
2013-08-28 20:32:11 +0200

    Changed the XSTR pattern to be able to handle whitespace
    The pattern was provided by Goober5000

--- src/mm/tstring/util/TStringGlobals.java
+++ src/mm/tstring/util/TStringGlobals.java
@@ -1,41 +1,36 @@
 package mm.tstring.util;
 
+import mm.tstring.objects.TString;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import mm.tstring.objects.TString;
-
 /**
  * Class holding all values that are used in the whole generator.
- * 
+ *
  * @author m!m
  */
-public class TStringGlobals
-{
-    public final static String        applicationName    = "@APP_NAME@";
-
+public class TStringGlobals {
+    public final static String applicationName = "@APP_NAME@";
     /**
-     * The version of the genrator
+     * The version of the generator
      */
-    public final static String        applicationVersion = "@APP_VERSION@";
-
+    public final static String applicationVersion = "@APP_VERSION@";
     /**
-     * The program will search for the files in these direcories
+     * The program will search for the files in these directories
      */
-    public static final String[]      searchDirs         = { "/data/tables",
-            "/data/missions"                            };
-
+    public static final String[] searchDirs = {"/data/tables",
+            "/data/missions"};
     /**
      * The regexp-Pattern to match a XSTR-statement (thanks to Spicious).
-     * Currently unused
+     * Updated version provided by Goober5000
      */
-    public static final Pattern       tstringPattern     =
-                                                                 Pattern.compile("XSTR\\(\"([^\"]*)\", ?(-?\\d+)\\)");
-
+    public static final Pattern tstringPattern =
+            Pattern.compile("XSTR\\s*\\(\\s*\"([^\"]*)\"\\s*,\\s*(-?\\d+)\\s*\\)");
     /**
      * The list of currently known TStrings
      */
-    public static final List<TString> tstrings           =
-                                                                 new ArrayList<TString>();
+    public static final List<TString> tstrings =
+            new ArrayList<TString>();
 }
