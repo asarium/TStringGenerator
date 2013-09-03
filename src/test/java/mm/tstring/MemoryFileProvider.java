2094e06a48ddf8a147e99b5a582e5e7995612251
asarium <asarium@gmail.com>
2013-09-03 15:17:08 +0200

    Make MemoryFileProvider compatible with the new interface signature.

--- src/test/java/mm/tstring/MemoryFileProvider.java
+++ src/test/java/mm/tstring/MemoryFileProvider.java
@@ -11,14 +11,29 @@ public class MemoryFileProvider implements IFileProvider
 {
     private Iterable<IFile> files;
 
-    public MemoryFileProvider(Iterable<IFile> files)
+    private IFile tstringTable;
+
+    public MemoryFileProvider(IFile tstringTable, Iterable<IFile> files)
     {
+        this.tstringTable = tstringTable;
         this.files = files;
+    }
+
+    @Override
+    public boolean backupFiles()
+    {
+        return true;
     }
 
     @Override
     public Iterable<IFile> getFiles()
     {
         return files;
+    }
+
+    @Override
+    public IFile getTStringTable()
+    {
+        return tstringTable;
     }
 }
