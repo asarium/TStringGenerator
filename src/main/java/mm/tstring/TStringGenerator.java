2c110444a98821fbb88af1357329758aa49dea24
asarium <asarium@gmail.com>
2013-09-03 15:17:57 +0200

    Removed old classes no longer needed.

--- src/main/java/mm/tstring/TStringGenerator.java
+++ src/main/java/mm/tstring/TStringGenerator.java
@@ -1,12 +1,6 @@
 package mm.tstring;
 
 import mm.tstring.config.TStringConfig;
-import mm.tstring.config.TStringConfig.Mode;
-import mm.tstring.ops.TStringsCreator;
-import mm.tstring.ops.TStringsUpdater;
-import mm.tstring.util.Util;
-
-import java.io.File;
 
 /**
  * @author m!m
@@ -17,25 +11,5 @@ public class TStringGenerator
     public static void main(String[] args)
     {
         TStringConfig.initialize(args);
-
-        File[] parseArray = null;
-
-        if (TStringConfig.getRootDir().exists())
-        {
-            parseArray = Util.getParseFiles();
-        }
-
-        if (parseArray == null || parseArray.length < 1)
-        {
-            System.out.println("No files found that have to be parsed.\nNothing to be done...");
-        }
-        else if (TStringConfig.getMode() == Mode.CREATE)
-        {
-            new TStringsCreator(TStringConfig.getRootDir(), TStringConfig.getBackupDir()).makeTString(parseArray);
-        }
-        else if (TStringConfig.getMode() == Mode.UPDATE)
-        {
-            new TStringsUpdater(TStringConfig.getRootDir(), TStringConfig.getBackupDir()).makeTString(parseArray);
-        }
     }
 }
