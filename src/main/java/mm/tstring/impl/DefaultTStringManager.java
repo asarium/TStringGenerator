f3fa01ea28157067c40108aa936a2c93c3436ec9
asarium <asarium@gmail.com>
2018-03-17 18:39:17 +0100

    Fix bug which would cause table corruption

--- src/main/java/mm/tstring/impl/DefaultTStringManager.java
+++ src/main/java/mm/tstring/impl/DefaultTStringManager.java
@@ -17,24 +17,16 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class DefaultTStringManager implements ITStringManager
-{
-    private static class TStringIndexComparator implements Comparator<TString>
-    {
+public class DefaultTStringManager implements ITStringManager {
+    private static class TStringIndexComparator implements Comparator<TString> {
 
         @Override
-        public int compare(TString o1, TString o2)
-        {
-            if (o1 == o2)
-            {
+        public int compare(TString o1, TString o2) {
+            if (o1 == o2) {
                 return 0;
-            }
-            else if (o1 == null)
-            {
+            } else if (o1 == null) {
                 return -1;
-            }
-            else if (o2 == null)
-            {
+            } else if (o2 == null) {
                 return 1;
             }
 
@@ -42,22 +34,15 @@ public class DefaultTStringManager implements ITStringManager
         }
     }
 
-    private static class TStringValueComparator implements Comparator<TString>
-    {
+    private static class TStringValueComparator implements Comparator<TString> {
 
         @Override
-        public int compare(TString o1, TString o2)
-        {
-            if (o1 == o2)
-            {
+        public int compare(TString o1, TString o2) {
+            if (o1 == o2) {
                 return 0;
-            }
-            else if (o1 == null)
-            {
+            } else if (o1 == null) {
                 return -1;
-            }
-            else if (o2 == null)
-            {
+            } else if (o2 == null) {
                 return 1;
             }
 
@@ -75,20 +60,16 @@ public class DefaultTStringManager implements ITStringManager
 
     private NavigableSet<TString> valueSortedTStrings;
 
-    public DefaultTStringManager(TStringConfig.Mode mode)
-    {
+    public DefaultTStringManager(TStringConfig.Mode mode) {
         updateExisitingTable = mode == TStringConfig.Mode.UPDATE;
     }
 
     @Override
-    public void collectTStrings(ITstringParser parser, IFileProvider fileProvider)
-    {
-        if (parser == null)
-        {
+    public void collectTStrings(ITstringParser parser, IFileProvider fileProvider) {
+        if (parser == null) {
             throw new IllegalArgumentException("parser");
         }
-        if (fileProvider == null)
-        {
+        if (fileProvider == null) {
             throw new IllegalArgumentException("fileProvider");
         }
 
@@ -100,37 +81,28 @@ public class DefaultTStringManager implements ITStringManager
     /**
      * Searches through indexSortedTStrings and fills index gaps
      */
-    private void fillGaps()
-    {
+    private void fillGaps() {
         // A new set needs to be filled in as entries may change their position
         NavigableSet<TString> newSet = new TreeSet<TString>(new TStringIndexComparator());
 
         int lastIndex = -1;
-        for (TString string : indexSortedTStrings)
-        {
-            if (string.isImmutable())
-            {
+        for (TString string : indexSortedTStrings) {
+            if (string.isImmutable()) {
                 // We can't change the string so simply add it but don't change lastIndex
                 newSet.add(string);
-            }
-            else
-            {
+            } else {
                 int diff = string.getIndex() - lastIndex;
 
-                if (diff > 1)
-                {
+                if (diff > 1) {
                     // We have a gap
                     int index = lastIndex;
 
                     // Increment index until we find the right spot
-                    do
-                    {
+                    do {
                         index++;
                         string.setIndex(index);
                     } while (!newSet.add(string));
-                }
-                else
-                {
+                } else {
                     newSet.add(string);
                 }
 
@@ -141,12 +113,10 @@ public class DefaultTStringManager implements ITStringManager
         indexSortedTStrings = newSet;
     }
 
-    private Collection<FileTString> parseTStrings(ITstringParser parser, IFileProvider fileProvider)
-    {
+    private Collection<FileTString> parseTStrings(ITstringParser parser, IFileProvider fileProvider) {
         Collection<FileTString> fileStrings = new ArrayList<FileTString>();
 
-        for (IFile file : fileProvider.getFiles())
-        {
+        for (IFile file : fileProvider.getFiles()) {
             logger.info("Parsing file: " + file.getName());
             Collection<FileTString> fileTStrings = parser.parseStrings(file);
 
@@ -157,24 +127,20 @@ public class DefaultTStringManager implements ITStringManager
     }
 
     @Override
-    public Collection<TString> getTStrings()
-    {
+    public Collection<TString> getTStrings() {
         return indexSortedTStrings;
     }
 
     @Override
-    public void writeStrings(ITstringParser parser, IFileProvider fileProvider)
-    {
-        if (!fileProvider.backupFiles())
-        {
+    public void writeStrings(ITstringParser parser, IFileProvider fileProvider) {
+        if (!fileProvider.backupFiles()) {
             logger.warn("Failed to create backups, no files will be changed.");
             return;
         }
 
         Map<IFile, Collection<FileTString>> tstringMapping = new HashMap<IFile, Collection<FileTString>>();
 
-        for (IFile file : fileProvider.getFiles())
-        {
+        for (IFile file : fileProvider.getFiles()) {
             tstringMapping.put(file, parser.parseStrings(file));
         }
 
@@ -182,46 +148,47 @@ public class DefaultTStringManager implements ITStringManager
 
         IFile tstringsTable = fileProvider.getTStringTable(true);
 
-        if (tstringsTable != null)
-        {
+        if (tstringsTable != null) {
             writeTable(tstringsTable);
-        }
-        else
-        {
+        } else {
             logger.info("Failed to create tstring.tbl!");
         }
     }
 
-    private void replaceContents(Map<IFile, Collection<FileTString>> tstringMapping)
-    {
-        for (Map.Entry<IFile, Collection<FileTString>> entry : tstringMapping.entrySet())
-        {
+    private void replaceContents(Map<IFile, Collection<FileTString>> tstringMapping) {
+        for (Map.Entry<IFile, Collection<FileTString>> entry : tstringMapping.entrySet()) {
             logger.info("Replacing strings in \"" + entry.getKey().getName() + "\".");
 
-            try
-            {
+            try {
                 StringBuilder contentBuilder = new StringBuilder(entry.getKey().getContent());
 
                 long currentOffset = 0;
 
-                for (FileTString tstring : entry.getValue())
-                {
+                List<FileTString> sortedTStrings = new ArrayList<>(entry.getValue());
+                sortedTStrings.sort((o1, o2) -> (int) (o1.getIndexOffset() - o2.getIndexOffset()));
+
+                for (FileTString tstring : sortedTStrings) {
                     NavigableSet<TString> headSet = valueSortedTStrings.headSet(tstring, true);
 
                     TString last = headSet.last();
 
-                    if (!last.getValue().equals(tstring.getValue()))
-                    {
+                    if (!last.getValue().equals(tstring.getValue())) {
                         logger.warn(
                                 "TString with content '" + tstring.getValue() + "' and index " + tstring.getIndex() +
                                         " wasn't found in parsed TString list!");
-                    }
-                    else
-                    {
+                    } else {
                         String newContent = Integer.toString(last.getIndex());
 
                         int begin = (int) (tstring.getIndexOffset() + currentOffset);
                         int end = (int) (begin + tstring.getIndexLength());
+
+                        String index = contentBuilder.substring(begin, end);
+
+                        try {
+                            Integer.parseInt(index);
+                        } catch (NumberFormatException e) {
+                            System.out.println();
+                        }
 
                         // Recompute offset
                         currentOffset = currentOffset + newContent.length() - tstring.getIndexLength();
@@ -231,68 +198,52 @@ public class DefaultTStringManager implements ITStringManager
                 }
 
                 entry.getKey().writeContent(contentBuilder.toString());
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 logger.warn("Failed to update contents of file '" + entry.getKey().getName() + "'.", e);
             }
         }
     }
 
-    private void writeTable(IFile tStringTable)
-    {
+    private void writeTable(IFile tStringTable) {
         PrintStream stream = null;
-        try
-        {
+        try {
             stream = new PrintStream(tStringTable.openOutputStream(), true);
 
             stream.println("#default");
-            for (TString string : indexSortedTStrings)
-            {
+            for (TString string : indexSortedTStrings) {
                 stream.printf("%d, \"%s\"%n%n", string.getIndex(), string.getValue());
             }
             stream.println("#end");
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             logger.error("Error while writing TStrings table!", e);
-        }
-        finally
-        {
-            if (stream != null)
-            {
+        } finally {
+            if (stream != null) {
                 stream.close();
             }
         }
     }
 
-    private void buildTStringsSets(Collection<FileTString> fileStrings, IFileProvider fileProvider)
-    {
+    private void buildTStringsSets(Collection<FileTString> fileStrings, IFileProvider fileProvider) {
         // We use a special comparator to make two TStrings equal when their contents match
         valueSortedTStrings = new TreeSet<TString>(new TStringValueComparator());
         indexSortedTStrings = new TreeSet<TString>(new TStringIndexComparator());
 
-        if (updateExisitingTable)
-        {
+        if (updateExisitingTable) {
             readExisitingTable(fileProvider.getTStringTable(false));
         }
 
-        for (FileTString fileTString : fileStrings)
-        {
+        for (FileTString fileTString : fileStrings) {
             // Add a clean TString instance here
             addTString(new TString(fileTString.getValue(), fileTString.getIndex()));
         }
     }
 
-    private void addTString(TString string)
-    {
-        if (string.isImmutable())
-        {
+    private void addTString(TString string) {
+        if (string.isImmutable()) {
             throw new IllegalArgumentException("TString may not be immutable!");
         }
 
-        if (string.getIndex() < 0)
-        {
+        if (string.getIndex() < 0) {
             string.setIndex(findNextFreeIndex());
         }
 
@@ -300,19 +251,15 @@ public class DefaultTStringManager implements ITStringManager
         boolean valueContained = valueSortedTStrings.contains(string);
         boolean indexContained = indexSortedTStrings.contains(string);
 
-        if (!valueContained && !indexContained)
-        {
+        if (!valueContained && !indexContained) {
             // This is a completely new string
             valueSortedTStrings.add(string);
             indexSortedTStrings.add(string);
-        }
-        else if (valueContained && !indexContained && string.getIndex() != -1)
-        {
+        } else if (valueContained && !indexContained && string.getIndex() != -1) {
             NavigableSet<TString> stringHeadSet = valueSortedTStrings.headSet(string, true);
             logger.warn("Found duplicate string '{}' with different index {} and {}. Keeping first...",
                     string.getValue(), stringHeadSet.last().getIndex(), string.getIndex());
-        }
-        else if (!valueContained) // indexContained is always true here
+        } else if (!valueContained) // indexContained is always true here
         {
             logger.warn("Found duplicate index usage of {}. Fixing...", string.getIndex());
 
@@ -322,9 +269,7 @@ public class DefaultTStringManager implements ITStringManager
             // Now add it
             valueSortedTStrings.add(string);
             indexSortedTStrings.add(string);
-        }
-        else
-        {
+        } else {
             // both already contained, simply check for errors
             NavigableSet<TString> valueHeadSet = valueSortedTStrings.headSet(string, true);
             NavigableSet<TString> indexHeadSet = indexSortedTStrings.headSet(string, true);
@@ -332,11 +277,10 @@ public class DefaultTStringManager implements ITStringManager
             TString sameValue = valueHeadSet.last();
             TString sameIndex = indexHeadSet.last();
 
-            if (!sameValue.equals(sameIndex))
-            {
+            if (!sameValue.equals(sameIndex)) {
                 logger.warn("Found mismatching TStrings!\n" +
-                        "String 1: XSTR(\"{}\", {})\n" +
-                        "String 2: XSTR(\"{}\", {})", sameIndex.getValue(), sameIndex.getIndex(), sameValue.getValue(),
+                                "String 1: XSTR(\"{}\", {})\n" +
+                                "String 2: XSTR(\"{}\", {})", sameIndex.getValue(), sameIndex.getIndex(), sameValue.getValue(),
                         sameValue.getIndex());
             }
         }
@@ -347,15 +291,12 @@ public class DefaultTStringManager implements ITStringManager
      *
      * @return The next free index
      */
-    private int findNextFreeIndex()
-    {
+    private int findNextFreeIndex() {
         int lastIndex = -1;
-        for (TString current : indexSortedTStrings)
-        {
+        for (TString current : indexSortedTStrings) {
             int diff = current.getIndex() - lastIndex;
 
-            if (diff > 1)
-            {
+            if (diff > 1) {
                 return lastIndex + 1;
             }
 
@@ -365,23 +306,17 @@ public class DefaultTStringManager implements ITStringManager
         return lastIndex + 1;
     }
 
-    private void readExisitingTable(IFile tStringTable)
-    {
-        if (tStringTable == null)
-        {
+    private void readExisitingTable(IFile tStringTable) {
+        if (tStringTable == null) {
             logger.warn("No tstrings.tbl file found, going to create mode...");
             updateExisitingTable = false;
-        }
-        else
-        {
-            try
-            {
+        } else {
+            try {
                 String content = tStringTable.getContent();
 
                 Matcher matcher = tstringTableEntryPattern.matcher(content);
 
-                while (matcher.find())
-                {
+                while (matcher.find()) {
                     int index = Integer.parseInt(matcher.group(1));
                     String contentString = matcher.group(2);
 
@@ -389,9 +324,7 @@ public class DefaultTStringManager implements ITStringManager
                     valueSortedTStrings.add(string);
                     indexSortedTStrings.add(string);
                 }
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 logger.warn("Failed to read tstrings contents, going to create mode.", e);
             }
         }
